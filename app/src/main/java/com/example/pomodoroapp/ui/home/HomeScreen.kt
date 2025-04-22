ee4eac9dbc11d748485be944e09f864f2456cda2
fisher.tai <unifisher02@gmail.com>
2025-04-22 23:03:00 +0800

    feat - [HomeScreen] HomeViewModel - 倒計時

--- app/src/main/java/com/example/pomodoroapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/pomodoroapp/ui/home/HomeScreen.kt
@@ -13,19 +13,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.pomodoroapp.util.PauseAbleCountDownTimer
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 
 @Composable
@@ -34,31 +28,6 @@ fun HomeScreen(modifier: Modifier = Modifier, homeViewModel: HomeViewModel = Hom
     val taskDescribe by homeViewModel.taskDescribe.collectAsStateWithLifecycle()
     val timeDisplay by homeViewModel.timeDisplay.collectAsStateWithLifecycle()
 
-    //倒數計時器
-    val countDownTimer = remember(homeViewModel.defaultTimeDuration) {
-        PauseAbleCountDownTimer(
-            millisInFuture = homeViewModel.defaultTimeDuration.inWholeMilliseconds,
-            countDownInterval = 1000,
-            onTick = { millis ->
-                homeViewModel.setTimeLeft(millis.toDuration(DurationUnit.MILLISECONDS))
-            },
-            onComplete = {
-
-            },
-            onCancel = {
-
-            },
-        )
-    }
-
-    //運行狀態
-    LaunchedEffect(countDownState) {
-        when (countDownState) {
-            CountDownState.RUNNING -> countDownTimer.start()
-            CountDownState.PAUSE -> countDownTimer.pause()
-            CountDownState.STOP -> countDownTimer.cancel()
-        }
-    }
     // UI實作
     Column(modifier.fillMaxSize()) {
         //元件1：Column擺放格式化後的Timer和任務描述
@@ -91,11 +60,7 @@ fun HomeScreen(modifier: Modifier = Modifier, homeViewModel: HomeViewModel = Hom
             Button(
                 shape = MaterialTheme.shapes.medium,
                 onClick = {
-                    val state = when (countDownState) {
-                        CountDownState.RUNNING -> CountDownState.PAUSE
-                        else -> CountDownState.RUNNING
-                    }
-                    homeViewModel.setCountDownState(state)
+                    homeViewModel.onClickEvent(HomeClickEvent.StartPauseClicked)
                 }
             ) {
                 Text(
@@ -111,8 +76,7 @@ fun HomeScreen(modifier: Modifier = Modifier, homeViewModel: HomeViewModel = Hom
                 shape = MaterialTheme.shapes.medium,
                 border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurfaceVariant),
                 onClick = {
-                    homeViewModel.resetTimer()
-                    homeViewModel.setCountDownState(CountDownState.STOP)
+                    homeViewModel.onClickEvent(HomeClickEvent.ResetClicked)
                 }) {
                 Text(
                     text = "Reset",
--- app/src/main/java/com/example/pomodoroapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/pomodoroapp/ui/home/HomeViewModel.kt
@@ -3,6 +3,8 @@ package com.example.pomodoroapp.ui.home
 import android.text.format.DateUtils
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pomodoroapp.util.TimerUtils
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -10,28 +12,21 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
-import kotlin.time.Duration
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.minutes
-
-data class HomeUiState(
-    val timeString: String = "25:00",
-    val countDownState: CountDownState = CountDownState.STOP,
-    val taskDescribe: String = "測試任務"
-)
+import kotlin.time.toDuration
 
 // 2. UI Event
-//sealed class HomeEvent {
-//    object StartPauseClicked : HomeEvent()
-//    object ResetClicked : HomeEvent()
-//}
+sealed class HomeClickEvent {
+    data object StartPauseClicked : HomeClickEvent()
+    data object ResetClicked : HomeClickEvent()
+}
 
 class HomeViewModel : ViewModel() {
 
     // 用Duration替代timestamp表示
-    val defaultTimeDuration = 25.minutes
-
-    private val _uiState = MutableStateFlow(HomeUiState())
-    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+    private val defaultTimeDuration = 25.minutes
+    private var countdownJob: Job? = null
 
     private val _countDownState = MutableStateFlow(CountDownState.STOP)
     val countDownState: StateFlow<CountDownState> = _countDownState.asStateFlow()
@@ -40,15 +35,6 @@ class HomeViewModel : ViewModel() {
     val taskDescribe: StateFlow<String> = _taskDescribe.asStateFlow()
 
     private val _timeLeft = MutableStateFlow(defaultTimeDuration)
-    val timeLeft: StateFlow<Duration> = _timeLeft.asStateFlow()
-
-    fun setTimeLeft(timeLeft: Duration) {
-        _timeLeft.update { timeLeft }
-    }
-
-    fun resetTimer(){
-        setTimeLeft(defaultTimeDuration)
-    }
 
     val timeDisplay: StateFlow<String> = _timeLeft
         .map { seconds ->
@@ -60,61 +46,38 @@ class HomeViewModel : ViewModel() {
             initialValue = DateUtils.formatElapsedTime(_timeLeft.value.inWholeSeconds)
         )
 
-    fun setCountDownState(state: CountDownState){
-        _countDownState.update { state }
+    fun onClickEvent(event: HomeClickEvent) {
+        when (event) {
+            HomeClickEvent.StartPauseClicked -> toggleStartPause()
+            HomeClickEvent.ResetClicked -> resetCountDown()
+        }
     }
 
-    fun pause(){
-        _countDownState.update { CountDownState.PAUSE }
+    private fun toggleStartPause() {
+        if (countDownState.value == CountDownState.RUNNING) {
+            countdownJob?.cancel()
+            _countDownState.update { CountDownState.PAUSE }
+        } else {
+            startTimer()
+            _countDownState.update { CountDownState.RUNNING }
+        }
     }
 
-    fun start(){
-        _countDownState.update { CountDownState.RUNNING }
+    private fun startTimer() {
+        countdownJob = viewModelScope.launch {
+            TimerUtils.countdownFLow(_timeLeft.value.inWholeMilliseconds, 1000)
+                .map { timeLeft -> timeLeft.toDuration(kotlin.time.DurationUnit.MILLISECONDS) }
+                .collect { timeLeft ->
+                    _timeLeft.update { timeLeft }
+                }
+        }
     }
 
-
-
-//
-//    private var countdownJob: Job? = null
-
-//    fun onEvent(event: HomeEvent) {
-//        when (event) {
-//            HomeEvent.StartPauseClicked -> startTimer()
-////                toggleStartPause()
-//            HomeEvent.ResetClicked -> reset()
-//        }
-//    }
-
-//    private fun toggleStartPause() {
-////        val current = _uiState.value
-//        if (countDownState == CountDownState.RUNNING) {
-//            countdownJob?.cancel()
-//            _uiState.update { it.copy(countDownState = CountDownState.PAUSE) }
-//        } else {
-//            _uiState.update { it.copy(countDownState = CountDownState.RUNNING) }
-//            startTimer()
-//        }
-//    }
-
-//    private fun startTimer() {
-//        countdownJob = viewModelScope.launch {
-//            TimerUtils.countdownFLow(timeLeft.value.inWholeMilliseconds, 1000)
-//                .map { timeLeft -> timeLeft.toDuration(kotlin.time.DurationUnit.MILLISECONDS) }
-//                .collect { timeLeft ->
-//                    _timeLeft.update { timeLeft }
-//                }
-//        }
-//    }
-
-//    private fun reset() {
-//        countdownJob?.cancel()
-//        _uiState.update {
-//            it.copy(
-//                timeString = DateUtils.formatElapsedTime(25.minutes.inWholeSeconds),
-//                countDownState = CountDownState.STOP
-//            )
-//        }
-//    }
+    private fun resetCountDown() {
+        countdownJob?.cancel()
+        _timeLeft.update { defaultTimeDuration } // 重置時間
+        _countDownState.update { CountDownState.STOP }
+    }
 }
 
 enum class CountDownState {
